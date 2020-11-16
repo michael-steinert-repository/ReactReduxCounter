333511c84c2f8e88496fae44560145a6077857ec
Michael Steinert <michael.steinert.94@gmail.com>
2020-11-16 13:11:09 +0100

    Added Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from "./Counter";
+import {useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const count = useSelector( (state) => state.counter.count);
+    const voters = ["Michael", "Marie", "Bruno"];
+    return (
+        <div className="App">
+            <h1>Reudx Counter</h1>
+            <h2>Total Votes: {count}</h2>
+            {voters.map((voter) => <Counter name={voter}/>)}
+        </div>
+    );
 }
 
 export default App;
